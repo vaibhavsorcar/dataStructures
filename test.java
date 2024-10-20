5375b91681c51fbe3e35b830ea7aa1454c6e1f03
vaibhavsorcar <vaibhav.sorcar13@gmail.com>
2024-10-20 23:21:18 +0530

    commit message

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static double myPow(double x, int n) {
+       System.out.println("hi");
+    }
+    public static void main(String[] args) {
+        System.out.println(myPow(2, 10));
+    }
+}
