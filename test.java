4e141e11ab94cff4e5dd9a3d4e1fbba0fed9c845
vaibhavsorcar <vaibhav.sorcar13@gmail.com>
2024-10-20 23:23:04 +0530

    Deleted [test.java]

--- test.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Main {
-    public static double myPow(double x, int n) {
-       System.out.println("hi");
-    }
-    public static void main(String[] args) {
-        System.out.println(myPow(2, 10));
-    }
-}
